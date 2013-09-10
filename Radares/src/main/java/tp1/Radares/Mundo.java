2767ba0e1ce527c2c3ef236aeaf136c7368d407e
Nahuel Tori <nahueltori@gmail.com>
2013-09-09 21:43:44 -0300

    Clase (Hola) Mundo!

---
+++ Radares/src/main/java/tp1/Radares/Mundo.java
@@ -0,0 +1,13 @@
+package tp1.Radares;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mundo {
+
+	List<Pais> paises;
+	
+	public Mundo(){
+		paises = new ArrayList<Pais>();
+	}
+}
