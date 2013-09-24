69dc6ad239336c67df849ecee474dc586d0b92c8
Nahuel Tori <nahueltori@gmail.com>
2013-09-24 15:46:29 -0300

    Create MundoFactory.java

---
+++ Radares/src/main/java/tp1/Radares/MundoFactory.java
@@ -0,0 +1,58 @@
+package tp1.Radares;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class MundoFactory {
+	private FileInputStream entrada;
+	private BufferedReader buffer;
+	private String strLine;
+	Mundo mundo = new Mundo();
+	
+	public MundoFactory(){
+		entrada = null;
+		buffer = null;
+		strLine = null;
+	}
+	
+	public void abrirArchivo(String nombre){
+		try{
+			entrada = new FileInputStream(nombre);
+			buffer = new BufferedReader(new InputStreamReader(entrada));
+		}catch (Exception e){
+            System.err.println("Ocurrio un error: " + e.getMessage());
+		}
+	}
+	
+	public void leerArchivo(){
+		String[] datos = null;
+		try{			
+			while ( (strLine = buffer.readLine()) != null){
+				datos = strLine.split(" ");
+				if (datos[0].equals("e")){
+					procesar(datos[1], datos[2]);
+				} 
+			}
+		}catch (Exception e){
+			System.err.println("Ocurrio un error: " + e.getMessage());
+		}
+	}
+	
+	private void procesar(String paise, String limitrofe) {
+		mundo.cargarPaises(paise, limitrofe);
+	}
+
+	public void cerrarArchivo(){
+		try{
+			entrada.close();
+		}catch (Exception e){
+			System.err.println("Ocurrio un error: " + e.getMessage());
+		}
+	}
+
+	public Mundo getMundo() {
+		return mundo;
+	}
+
+}
